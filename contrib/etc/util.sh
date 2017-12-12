188cc946c43b1c3b79a4bb9157e64cf79740516a
Elexy <alex@seretalabs.com>
2017-12-12 18:08:03 +0100

    fixed syntax and early exit

--- contrib/etc/util.sh
+++ contrib/etc/util.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 CONFIG_PATH=.config/config.json
 
-function checkVersionIsSet() {
+checkVersionIsSet() {
     V=$(echo ${VERSION} | cut -d'.' -f1)
     if [ -z "$VERSION" ]; then
         echo "Error: VERSION not set"
         exit 1
     fi
-    exit 0
+    return $true
 }
 
-function getProjectSecret() {
+getProjectSecret() {
     checkVersionIsSet
     secret_varname=$(jq -r --arg version ${V} '.products[0].projects[] | select(.version==$version) | .secret_env_name' ${CONFIG_PATH})
     if [ ! -z "${!secret_varname}" ]; then
@@ -21,7 +21,7 @@ function getProjectSecret() {
     fi
 }
 
-function getProjectId() {
+getProjectId() {
     checkVersionIsSet
     jq -r --arg version ${V} '.products[0].projects[] | select(.version==$version) | .project_id' ${CONFIG_PATH}
 }
