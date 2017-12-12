500fa68240d3dc98f6e809b509919965f05ee3cc
Elexy <alex@seretalabs.com>
2017-12-12 07:50:41 +0100

    there is no VERSION.x in the env, so we have to use x.x.x and cut only the major off

--- getRepoSecret.sh
+++ getRepoSecret.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+V=$(echo ${VERSION} | cut -d'.' -f1)
 if [ -z "$VERSION" ]; then
     echo "Error: VERSION not set"
     exit 1
 fi
-secret_varname=$(cat .config/config.json | jq -r --arg version ${VERSION} '.products[0].projects[] | select(.version==$version) | .secret_env_name')
+ secret_varname=$(jq -r --arg version ${V} '.products[0].projects[] | select(.version==$version) | .secret_env_name' .config/config.json)
 if [ ! -z "${!secret_varname}" ]; then
     echo "${!secret_varname}"
 else
