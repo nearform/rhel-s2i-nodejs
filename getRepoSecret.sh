700bfec8631853b57315db6c9a4e0b5783bc7a1d
Elexy <alex@seretalabs.com>
2017-12-12 07:14:30 +0100

    add error checking and echo only var value on success

--- getRepoSecret.sh
+++ getRepoSecret.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
-secret_varname=$(jq -r --arg version ${VERSION} '.products[0].projects[] | select(.version==$version) | .secret_env_name' rhel-s2i-nodejs/.config/config.json)
+#!/bin/bash
+if [ -z "$VERSION" ]; then
+    echo "Error: VERSION not set"
+    exit 1
+fi
+secret_varname=$(cat .config/config.json | jq -r --arg version ${VERSION} '.products[0].projects[] | select(.version==$version) | .secret_env_name')
 if [ ! -z "${!secret_varname}" ]; then
-    echo "${secret_varname}: found"
     echo "${!secret_varname}"
 else
-    echo "${secret_varname}: not set"
+    echo "Error: ${secret_varname}: not set"
     exit 1
 fi
