6e18d30f1e6c8ce05a83afbb100619cb3b2ffe7a
Elexy <alex@seretalabs.com>
2017-12-20 08:36:22 -0500

    add another line for the tests

--- contrib/etc/install_node_source.sh
+++ contrib/etc/install_node_source.sh
@@ -11,6 +11,7 @@ else
     tar -zxf /src/node-v${NODE_VERSION}.tar.gz -C /tmp/ --strip-components=1
     cd /tmp/
     ./configure
+    make remark-preset-lint-node
     make lint-md-clean lint-md-build
     make -j4 test
     make -j -l8
